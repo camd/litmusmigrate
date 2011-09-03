645e9c7c9239d7b93ce28dbee10ef3cf3b2ad470
Cameron Dawson <cdawson@mozilla.com>
2011-09-02 18:08:48 -0700

    now working, with minimal CSS formatting, even

--- litmustocsv/forms.py
+++ litmustocsv/forms.py
@@ -5,4 +5,6 @@ from django.forms import ChoiceField, MultipleChoiceField
 class HomeForm(forms.Form):
 
     product = forms.ModelChoiceField(queryset=Products.objects.all().order_by('name'))
+    branch = forms.ModelChoiceField(queryset=Branches.objects.all().order_by('name'))
+    testgroups = forms.ModelMultipleChoiceField(queryset=Testgroups.objects.all().order_by('name'))
 
--- litmustocsv/views.py
+++ litmustocsv/views.py
@@ -4,9 +4,38 @@ from django.shortcuts import render_to_response
 from django.template import RequestContext
 from forms import HomeForm
 from models import Products, Branches, Testgroups
-import csv
+import csv, codecs, cStringIO
 from django.utils.simplejson import dumps, loads
 from django.core import serializers
+
+class UnicodeWriter:
+    """
+    A CSV writer which will write rows to CSV file "f",
+    which is encoded in the given encoding.
+    """
+
+    def __init__(self, f, dialect=csv.excel, encoding="utf-8", **kwds):
+        # Redirect output to a queue
+        self.queue = cStringIO.StringIO()
+        self.writer = csv.writer(self.queue, dialect=dialect, **kwds)
+        self.stream = f
+        self.encoder = codecs.getincrementalencoder(encoding)()
+
+    def writerow(self, row):
+        self.writer.writerow([unicode(s).encode("utf-8") for s in row])
+        # Fetch UTF-8 output from the queue ...
+        data = self.queue.getvalue()
+        data = data.decode("utf-8")
+        # ... and reencode it into the target encoding
+        data = self.encoder.encode(data)
+        # write to the target stream
+        self.stream.write(data)
+        # empty queue
+        self.queue.truncate(0)
+
+    def writerows(self, rows):
+        for row in rows:
+            self.writerow(row)
 
 def home(request):
     if request.method == 'POST': # If the form has been submitted...
@@ -16,8 +45,7 @@ def home(request):
             # ...
 
             product = form.cleaned_data['product']
-
-            branch_id = form.cleaned_data['branch']
+            branch_id = form.cleaned_data['branch'].branch_id
             testgroups = form.cleaned_data['testgroups']
             tg_ids = [str(x.testgroup_id) for x in testgroups]
 
@@ -28,7 +56,7 @@ def home(request):
             response = HttpResponse(mimetype='text/csv')
             response['Content-Disposition'] = 'attachment; filename=%s_litmusoutput.csv' % product.name
 
-            csv_writer = csv.writer(response)
+            csv_writer = UnicodeWriter(response)
             csv_writer.writerow([i[0] for i in cursor.description]) # write headers
             csv_writer.writerows(cursor)
             del csv_writer # this will close the CSV file
